3d66a489862278fa9ac1d31519c0c1140c083f57
floodliu <floodliu@gmail.com>
2016-04-18 14:06:46 +0800

    init repo and add project "BookPub".

---
+++ bookpub/src/main/java/com/example/BookPubApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookPubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookPubApplication.class, args);
+	}
+}
