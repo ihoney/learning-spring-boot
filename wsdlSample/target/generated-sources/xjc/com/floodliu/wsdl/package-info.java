0adfd683e8a3b6e49c7c104897c1aac37f21c409
floodliu <floodliu@gmail.com>
2016-04-21 20:33:16 +0800

    regenerate java classes for GetWeather wsdl service.

--- wsdlSample/target/generated-sources/xjc/com/floodliu/wsdl/package-info.java
+++ wsdlSample/target/generated-sources/xjc/com/floodliu/wsdl/package-info.java
@@ -2,7 +2,7 @@
 // 此文件是由 JavaTM Architecture for XML Binding (JAXB) 引用实现 v2.2.11 生成的
 // 请访问 <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // 在重新编译源模式时, 对此文件的所有修改都将丢失。
-// 生成时间: 2016.04.21 时间 05:30:37 PM CST 
+// 生成时间: 2016.04.21 时间 08:31:49 PM CST 
 //
 
 @javax.xml.bind.annotation.XmlSchema(namespace = "http://ws.cdyne.com/WeatherWS/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
