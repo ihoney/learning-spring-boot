73f296385ddc295dd1d2982d05ca3fd94a631a02
floodliu <floodliu@gmail.com>
2016-04-26 01:23:17 +0800

    configure H2 (in memory).

--- bookpub/src/main/java/com/example/bookpub/StartupRunner.java
+++ bookpub/src/main/java/com/example/bookpub/StartupRunner.java
@@ -1,7 +1,9 @@
 package com.example.bookpub;
 
+import org.apache.tomcat.jdbc.pool.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 
 /**
@@ -11,8 +13,12 @@ public class StartupRunner implements CommandLineRunner {
 
     private Logger logger = LoggerFactory.getLogger(StartupRunner.class);
 
+    @Autowired
+    private DataSource dataSource;
+
     @Override
     public void run(String... strings) throws Exception {
         logger.info("hello");
+        logger.info("Datasource: " + dataSource.toString());
     }
 }
