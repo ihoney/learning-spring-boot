b36c8cfb31273e29e07dfaee7ef2bdbab21b8c55
floodliu <floodliu@gmail.com>
2016-04-25 14:21:32 +0800

    add project uicDemo.

---
+++ uicDemo/src/main/java/com/example/UicDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UicDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UicDemoApplication.class, args);
+	}
+}
