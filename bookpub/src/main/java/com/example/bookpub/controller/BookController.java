e4fca59d5fbb7246f958a87da41622f7b6166681
floodliu <floodliu@gmail.com>
2016-04-26 14:06:28 +0800

    add property editor.

--- bookpub/src/main/java/com/example/bookpub/controller/BookController.java
+++ bookpub/src/main/java/com/example/bookpub/controller/BookController.java
@@ -1,12 +1,11 @@
 package com.example.bookpub.controller;
 
 import com.example.bookpub.entity.Book;
+import com.example.bookpub.formatter.IsbnEditor;
 import com.example.bookpub.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by hongtao on 16/4/26.
@@ -26,5 +25,10 @@ public class BookController {
     @RequestMapping(value = "/{isbn}", method = RequestMethod.GET)
     public Book getBook(@PathVariable("isbn") String isbn) {
         return bookRepository.findBookByIsbn(isbn);
+    }
+
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        binder.registerCustomEditor(IsbnEditor.class, new IsbnEditor());
     }
 }
