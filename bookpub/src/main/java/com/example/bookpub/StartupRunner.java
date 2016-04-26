c2ae6c685a09ab1721ed45b7bda8de3116b6d8dc
floodliu <floodliu@gmail.com>
2016-04-26 11:32:48 +0800

    add scheduled executor.

--- bookpub/src/main/java/com/example/bookpub/StartupRunner.java
+++ bookpub/src/main/java/com/example/bookpub/StartupRunner.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.scheduling.annotation.Scheduled;
 
 /**
  * Created by hongtao on 16/4/26.
@@ -24,6 +25,11 @@ public class StartupRunner implements CommandLineRunner {
     public void run(String... strings) throws Exception {
         logger.info("hello");
         logger.info("Datasource: " + dataSource.toString());
+        logger.info("Number of books: " + bookRepository.count());
+    }
+
+    @Scheduled(initialDelay = 1000, fixedRate = 10000)
+    public void run() {
         logger.info("Number of books: " + bookRepository.count());
     }
 }
