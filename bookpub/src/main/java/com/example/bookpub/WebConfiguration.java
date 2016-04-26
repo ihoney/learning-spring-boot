a0d2c2e724df09d444f7f7e4def0da7136b0be7a
floodliu <floodliu@gmail.com>
2016-04-26 13:51:51 +0800

    remove useless headers of message converters.

--- bookpub/src/main/java/com/example/bookpub/WebConfiguration.java
+++ bookpub/src/main/java/com/example/bookpub/WebConfiguration.java
@@ -4,12 +4,9 @@ import org.apache.catalina.filters.RemoteIpFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.ByteArrayHttpMessageConverter;
-import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
-
-import java.util.List;
 
 /**
  * Created by hongtao on 16/4/26.
