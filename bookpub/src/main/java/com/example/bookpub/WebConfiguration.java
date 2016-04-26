e314a1488a58d2a90c273bfffe4d52eee9b45bf1
floodliu <floodliu@gmail.com>
2016-04-26 23:49:33 +0800

    add static resource handler.

--- bookpub/src/main/java/com/example/bookpub/WebConfiguration.java
+++ bookpub/src/main/java/com/example/bookpub/WebConfiguration.java
@@ -10,6 +10,7 @@ import org.springframework.format.FormatterRegistry;
 import org.springframework.http.converter.ByteArrayHttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
@@ -62,4 +63,9 @@ public class WebConfiguration extends WebMvcConfigurerAdapter {
 //    public void configurePathMatch(PathMatchConfigurer configurer) {
 //        configurer.setUseSuffixPatternMatch(false).setUseTrailingSlashMatch(true);
 //    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/internal/**").addResourceLocations("classpath:/");
+    }
 }
