26b147e42c6dad5fd8bcaf11d1e1bd6696febedf
floodliu <floodliu@gmail.com>
2016-04-26 12:20:07 +0800

    add interceptor.

--- bookpub/src/main/java/com/example/bookpub/WebConfiguration.java
+++ bookpub/src/main/java/com/example/bookpub/WebConfiguration.java
@@ -3,15 +3,28 @@ package com.example.bookpub;
 import org.apache.catalina.filters.RemoteIpFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 /**
  * Created by hongtao on 16/4/26.
  */
 @Configuration
-public class WebConfiguration {
+public class WebConfiguration extends WebMvcConfigurerAdapter {
 
     @Bean
     public RemoteIpFilter remoteIpFilter() {
         return new RemoteIpFilter();
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        return new LocaleChangeInterceptor();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
     }
 }
