0cdfa01f1fc0c3de55af711652bb6f9b5453a368
floodliu <floodliu@gmail.com>
2016-05-29 01:24:42 +0800

    add a demo of adding-a-interceptor.

---
+++ interceptor/src/main/java/com/example/configuration/WebConfiguration.java
@@ -0,0 +1,24 @@
+package com.example.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+/**
+ * Created by floodliu on 16/5/29.
+ */
+@Configuration
+public class WebConfiguration extends WebMvcConfigurerAdapter {
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        return new LocaleChangeInterceptor();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+}
